8991a79f70b2e28dc6f1ce7bfed623ddd336b5fa
Timotej <timotej.bojarovski@gmail.com>
2022-03-20 18:38:48 +0100

    Extra Homework assignment

---
+++ Sedc.OOP.Class5.ExtraHomework/Sedc.OOP.Class5.ExtraHomework.App/Modes/Easy.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sedc.OOP.Class5.ExtraHomework.App.Modes
+{
+    public class Easy
+    {
+        public static void Game()
+        {
+            Random random = new Random();
+            int n = random.Next(1, 100);
+            int guess = 0;
+            Console.WriteLine("You choose Easy difficulty: you have 9 tries left to guess my number! (1-100)");
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine("Input your guess right here:");
+                string input = Console.ReadLine();
+                guess = int.Parse(input);
+                if (guess == n && i == 1)
+                {
+                    Console.WriteLine("!!! Lucky guess !!!");
+                    break;
+                }
+                else if (guess == n)
+                {
+                    Console.WriteLine("You got it right!");
+                    break;
+                }
+                else if (n > guess && n - guess < 3)
+                {
+                    Console.WriteLine($"You're so close, but you need to go higher. Number you inputed was: {guess}!");
+                }
+                else if (n > guess && n - guess < 10)
+                {
+                    Console.WriteLine($"Need to go higher. Number you inputed was: {guess}!");
+                }
+                else if (n > guess)
+                {
+                    Console.WriteLine("Go higher");
+                }
+                else if (n < guess && guess - n < 3)
+                {
+                    Console.WriteLine($"You're so close, but you need to go lower. Number you inputed was: {guess}!");
+                }
+                else if (n < guess && guess - n < 10)
+                {
+                    Console.WriteLine($"Need to go lower. Number you inputed was: {guess}!");
+                }
+                else if (n < guess)
+                {
+                    Console.WriteLine("Go lower");
+                }
+                else if (guess != n && i == 9)
+                {
+                    Console.WriteLine("Sorry, would you like to try again?");
+                }
+            }
+            Console.WriteLine($"The number was {n}");
+        }
+    }
+}
---
+++ Sedc.OOP.Class5.ExtraHomework/Sedc.OOP.Class5.ExtraHomework.App/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Sedc.OOP.Class5.ExtraHomework.App.Modes;
+namespace Sedc.OOP.Class5.ExtraHomework.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enert the game mode you want: Easy, Medium or Hard");
+            string difficulty = Console.ReadLine();
+            difficulty = difficulty.ToLower();
+            if (difficulty == "easy")
+            {
+                Easy.Game();
+            }
+            else if (difficulty == "medium")
+            {
+                Medium.Game();
+            }
+            else if (difficulty == "hard")
+            {
+                Hard.Game();
+            }
+        }
+    }
+}
