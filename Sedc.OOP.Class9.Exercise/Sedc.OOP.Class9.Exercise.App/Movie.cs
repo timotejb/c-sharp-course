7d3274bee05dd01554e607e05c1f1dedcdb32706
Timotej Bojarovski <Timotej.Bojarovski@sedc.mk>
2022-03-28 20:55:58 +0200

    exercise

---
+++ Sedc.OOP.Class9.Exercise/Sedc.OOP.Class9.Exercise.App/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sedc.OOP.Class9.Exercise.App
+{
+    public class Movie
+    {
+        public string Title { get; set; }   
+        public Genre Genre { get; set; }   
+        public int Rating { get; set; }
+        public int TicketPrice { get; set; }
+
+        public Movie(string title, Genre genre, int rating)
+        {
+            Title = title;
+            Genre = genre;
+            if (rating < 1 && rating > 5)
+                throw new Exception("Invalid rating");
+            Rating = rating;
+            TicketPrice = rating*5;
+        }
+    }
+}
